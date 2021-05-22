6d4af59b0870a46572e586a0daa32e85e56e08e9
Oleg Trukhachov <trukhachov91@gmail.com>
2021-05-22 19:29:22 +0200

    com

--- src/js/read-more.js
+++ src/js/read-more.js
@@ -1,21 +1,58 @@
 
-$( "read-more-btn" ).first().click(function() {
-  $( "moreText" ).first().fadeToggle( "slow", "linear" );
-});
+          // $( "read-more-btn" ).first().click(function() {
+          //   $( "moreText" ).first().fadeToggle( "slow", "linear" );
+          // });
 
-
+      
 // $("button").last().click(function () {
 //   $( "p" ).last().fadeToggle( "fast", function() {
 //     $( "#dots" ).append( "<div>finished</div>" );
 //   });
 // });
 
-
-// $('read-more-btn').click(function () {s
-//     $('moreText').slideToggle();
-//     if ($('Read-more-btn').text() == "Less") {
+// function ShowHide() {
+//     $('ab-dots').slideToggle();
+//     if ($('read-more-btn').text() == "Less") {
 //         $(this).text("Read more")
 //     } else {
 //         $(this).text("Less")
 //     }
-// });
+// };
+
+// $('read-more-btn').click(function () {s
+//     $('ab-dots').slideToggle();
+//     if ($('read-more-btn').text() == "Less") {
+//         $(this).text("Read more")
+//     } else {
+//         $(this).text("Less")
+//     }
+// }
+// function myFunction() {
+//   var x = document.getElementById("ab-dots");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// };
+
+//  aria-expanded="false"
+//                 aria-controls="text-hidden"
+//                 aria-label="Read-more"
+//                 data-program__button
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+};
